5109cc837c5a7d0aed00508f7b2abeabebb5fb20
itmsuk01 <sukiasyan.samvel@it-motive.de>
2021-06-11 16:48:03 +0200

    project init

---
+++ src/main/java/com/example/telbook/TelbookApplication.java
@@ -0,0 +1,12 @@
+package com.example.telbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelbookApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/telbook/TelbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
